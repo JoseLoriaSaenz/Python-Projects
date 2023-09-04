94495687f2fe3a632d5a2cbc83cdefd54928a647
JoseLoriaSaenz <loriafjose@gmail.com>
2023-09-04 09:06:38 -0600

    New projects

---
+++ Greetings/greeting.py
@@ -0,0 +1,4 @@
+hello = "Hello"
+name = input("What's your name? Jose")
+greeting = hello + " " + name
+print(greeting)
--- TestPython/Hello.py
+++ TestPython/Hello.py
@@ -1,1 +1,2 @@
+# This is just to validate VS Code is using the python interpreter
 print('Hello World')
