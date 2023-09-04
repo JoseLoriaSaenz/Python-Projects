202f6ccffba7da85db51ee6b64e5a0cc28b03ce9
JoseLoriaSaenz <loriafjose@gmail.com>
2023-09-04 09:55:59 -0600

    New Commit

---
+++ Expenses/Expenses.py
@@ -0,0 +1,3 @@
+expenses = [10.4, 8, 5.65, 15, 20, 5.6, 3]
+
+print("Total expent $", sum(expenses), sep='')
---
+++ GroceriesList/groceries.py
@@ -0,0 +1,12 @@
+groceries = ["Milk", "Bread", "Bananas"]
+
+groceries.append("Coffee")
+
+print("Your basic groceries list contains:")
+print(groceries)
+
+newgrocery = input("Add new grocery to list: ")
+groceries.append(newgrocery)
+
+print("Your modified groceries list contains:")
+print(groceries)
